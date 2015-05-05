cdd7bcb8a9add0a71294a0d98f39fc2070a317ff
Christian Gebauer <mail@christiangebauer.de>
2015-05-05 03:34:03 +0200

    -added multicast-receiver capability -added small screen size optimized cdu font -parametrized character placement to make better use of available screen size -added escape-key to exit

--- python2_pygame_cdu_display.py
+++ python2_pygame_cdu_display.py
@@ -15,12 +15,25 @@ from __future__ import print_function
 # use UDP (configure a UDPSender in BIOSConfig.lua
 # to send the data to the host running this script)
 
+#CONNECTION = {
+#        "type":"UDP",
+#        "port":7777
+#}
+
 CONNECTION = {
-        "type":"UDP",
-        "port":7777
+        "type":"MULTICAST",
+        "group":"239.255.50.10",
+        "port":5010
 }
+
 CDU_COLOR = (0, 255, 0)
-CHARACTER_SIZE = 12
+CHARACTER_WIDTH = 16
+CHARACTER_HEIGHT = 16
+
+KERNING = 3
+LINE_SPACING = 6
+
+SOURCE_SIZE = 16
 
 import socket
 import sys
@@ -28,9 +41,8 @@ import struct
 import os
 from dcsbios import ProtocolParser, StringBuffer, IntegerBuffer
 
-if not os.path.isfile("font_A-10_CDU.tga"):
-        print("font_A-10_CDU.tga not found.")
-        print("Please copy this file from DCS World\mods\aircraft\A-10C\Cockpit\Resources\IndicationTextures")
+if not os.path.isfile("font_A-10_CDU_small.tga"):
+        print("font_A-10_CDU_small.tga not found.")
         print("press return to close")
         raw_input()
         sys.exit()
@@ -43,17 +55,29 @@ if CONNECTION["type"] == "TCP":
 elif CONNECTION["type"] == "UDP":
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.bind(("0.0.0.0", CONNECTION["port"]))
+elif CONNECTION["type"] == "MULTICAST":
+        #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(("0.0.0.0", CONNECTION["port"]))
+        group = socket.inet_aton(CONNECTION["group"])
+        mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
 s.settimeout(0)
         
 import pygame
 pygame.init()
-main_surface = pygame.display.set_mode((24*CHARACTER_SIZE,10*CHARACTER_SIZE))
-font_img = pygame.image.load("font_A-10_CDU.tga")  # 512x512 pixel, 8x8 characters
+main_surface = pygame.display.set_mode((24*(CHARACTER_WIDTH-KERNING),10*(CHARACTER_HEIGHT+LINE_SPACING)),pygame.FULLSCREEN)
+font_img = pygame.image.load("font_A-10_CDU_small.tga")  # 128x128 pixel, 8x8 characters
+
+pygame.mouse.set_visible(False)
+
 def get_subimg(index):
         assert index >= 0 and index <= 63
         row = index // 8
         col = index - row*8
-        img = font_img.subsurface(col*64, row*64, 64, 64)
+        img = font_img.subsurface(col*SOURCE_SIZE, row*SOURCE_SIZE, SOURCE_SIZE, SOURCE_SIZE)
         return img
 
 pos_map = {
@@ -122,18 +146,24 @@ font = {}
 
 for k in pos_map.keys():
         img = get_subimg(pos_map[k])
-        for x in range(64):
-                for y in range(64):
+        for x in range(SOURCE_SIZE):
+                for y in range(SOURCE_SIZE):
                         r,g,b,a = img.get_at((x,y))
-                        if a > 128:
+                        if a > SOURCE_SIZE*2:
                                 img.set_at((x,y), CDU_COLOR)
-        img = pygame.transform.scale(img, (CHARACTER_SIZE, CHARACTER_SIZE))
+        img = pygame.transform.scale(img, (CHARACTER_WIDTH, CHARACTER_HEIGHT))
         font[k] = img
+
+def finish():
+        pygame.mouse.set_visible(True)
+        pygame.quit()
+        #quit()
+        sys.exit()
 
 def set_char(line, column, c):
         if c not in font:
                 c = "?"
-        main_surface.blit(font[c], (CHARACTER_SIZE*column, CHARACTER_SIZE*line))
+        main_surface.blit(font[c], ((CHARACTER_WIDTH-KERNING)*column, (CHARACTER_HEIGHT+LINE_SPACING)*line))
 
 CDUDISPLAY_START_ADDRESS = 0x11c0
 cdu_display_data = bytearray(24*10)
@@ -150,7 +180,10 @@ while 1:
         pygame.event.pump()
         for ev in pygame.event.get():
                 if ev.type == pygame.QUIT:
-                        sys.exit()
+                        finish()
+                if ev.type == pygame.KEYDOWN:
+                        if ev.key == pygame.K_ESCAPE:
+                                finish()
 
         main_surface.fill((0, 0, 0))
 
